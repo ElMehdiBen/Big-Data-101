a232155c55d68992665d02782cefb8bc859f5f41
El Mehdi Benyoussef <elmehdibenyoussef@gmail.com>
2024-11-27 19:31:57 +0100

    Update script.sh

--- Nifi_Full/namenode/script.sh
+++ Nifi_Full/namenode/script.sh
@@ -1,5 +1,3 @@
-hadoop fs -chmod 777 /
-
 sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
 sudo sed -i -e '/^#baseurl=http:\/\/mirror.centos.org/p;s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 sudo yum update -y
@@ -20,13 +18,15 @@ mv zeppelin-0.11.0-bin-all /opt/zeppelin
 cp /opt/zeppelin/conf/zeppelin-env.sh.template /opt/zeppelin/conf/zeppelin-env.sh
 
 sudo wget https://archive.apache.org/dist/incubator/livy/0.7.1-incubating/apache-livy-0.7.1-incubating-bin.zip
-sudo unzip apache-livy-0.7.1-incubating-bin.zip
 mv apache-livy-0.7.1-incubating-bin /opt/livy
+cd /opt/livy
+sudo unzip apache-livy-0.7.1-incubating-bin.zip
 
 export HADOOP_CONF_DIR=/opt/hadoop/conf
 export JAVA_HOME=/usr/lib/jvm/jre/
 export ZEPPELIN_ADDR=0.0.0.0
 export SPARK_HOME=/opt/spark/spark-3.5.0-bin-hadoop3
+export SPARK_CONF_DIR=$SPARK_HOME/conf
 
 sudo /opt/zeppelin/bin/zeppelin-daemon.sh start
 sudo /opt/zeppelin/bin/zeppelin-daemon.sh status
