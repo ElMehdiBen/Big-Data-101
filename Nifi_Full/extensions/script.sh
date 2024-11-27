8e69687c51a20cda4e6191396dcb795134b518d2
El Mehdi Benyoussef <elmehdibenyoussef@gmail.com>
2024-11-27 18:29:05 +0100

    Create script.sh

---
+++ Nifi_Full/extensions/script.sh
@@ -0,0 +1,2 @@
+wget https://repo1.maven.org/maven2/org/apache/nifi/nifi-hadoop-nar/2.0.0/nifi-hadoop-nar-2.0.0.nar
+wget https://repo1.maven.org/maven2/org/apache/nifi/nifi-hadoop-libraries-nar/2.0.0/nifi-hadoop-libraries-nar-2.0.0.nar
