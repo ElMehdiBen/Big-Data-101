aa57bc90f3f1bb31634298dd7af34a9b75bc617f
El Mehdi Benyoussef <elmehdibenyoussef@gmail.com>
2024-11-27 19:11:31 +0100

    Update script.sh

--- Nifi_Full/namenode/script.sh
+++ Nifi_Full/namenode/script.sh
@@ -3,11 +3,12 @@ hadoop fs -chmod 777 /
 sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
 sudo sed -i -e '/^#baseurl=http:\/\/mirror.centos.org/p;s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 sudo yum update -y
+sudo yum install unzip -y
 
-sudo wget https://archive.apache.org/dist/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz --no-check-certificate
+sudo wget https://archive.apache.org/dist/spark/spark-2.4.8/spark-2.4.8-bin-hadoop2.7.tgz --no-check-certificate
 mkdir /opt/spark
-mv spark-3.5.0-bin-hadoop3.tgz /opt/spark/
-tar -zxvf /opt/spark/spark-3.5.0-bin-hadoop3.tgz -C /opt/spark
+mv spark-2.4.8-bin-hadoop2.7 /opt/spark/
+tar -zxvf /opt/spark/spark-2.4.8-bin-hadoop2.7 -C /opt/spark
 
 sudo yum install -y python3
 sudo unlink /usr/bin/python
@@ -18,9 +19,14 @@ tar zxvf zeppelin-0.11.0-bin-all.tgz
 mv zeppelin-0.11.0-bin-all /opt/zeppelin
 cp /opt/zeppelin/conf/zeppelin-env.sh.template /opt/zeppelin/conf/zeppelin-env.sh
 
+sudo wget https://archive.apache.org/dist/incubator/livy/0.7.1-incubating/apache-livy-0.7.1-incubating-bin.zip
+
+export HADOOP_CONF_DIR=/opt/hadoop/conf
 export JAVA_HOME=/usr/lib/jvm/jre/
 export ZEPPELIN_ADDR=0.0.0.0
 export SPARK_HOME=/opt/spark/spark-3.5.0-bin-hadoop3
 
-/opt/zeppelin/bin/zeppelin-daemon.sh start
-/opt/zeppelin/bin/zeppelin-daemon.sh status
+sudo /opt/zeppelin/bin/zeppelin-daemon.sh start
+sudo /opt/zeppelin/bin/zeppelin-daemon.sh status
+
+sudo ./bin/livy-server start
